f98586ce1431e5726fc6a34190dd58f1c9b0a681
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-04-05 13:32:11 +0300

    updated

--- scripts/analysoi.sh
+++ scripts/analysoi.sh
@@ -1,8 +1,9 @@
-echo "Detected languages:"
-find . -type f -name "*.py" | grep -q . && echo "- Python"
-find . -type f -name "*.java" | grep -q . && echo "- Java"
-find . -type f -name "*.js" | grep -q . && echo "- JavaScript"
-find . -type f -name "*.ts" | grep -q . && echo "- TypeScript"
+echo " ANALYSIS REPORT " > analysis_report.txt
+echo "Detected languages:" >> analysis_report.txt
+find . -type f -name "*.py" | grep -q . && echo "- Python" >> analysis_report.txt
+find . -type f -name "*.java" | grep -q . && echo "- Java" >> analysis_report.txt
+find . -type f -name "*.js" | grep -q . && echo "- JavaScript" >> analysis_report.txt
+find . -type f -name "*.ts" | grep -q . && echo "- TypeScript" >> analysis_report.txt
 
 echo "Design patterns detected:" > analysis_report.txt
 
@@ -11,3 +12,5 @@ grep -R "create[A-Z]" -n . && echo "- Factory Method" >> analysis_report.txt
 grep -R "notify" -n . && echo "- Observer" >> analysis_report.txt
 grep -R "Strategy" -n . && echo "- Strategy" >> analysis_report.txt
 grep -R "Decorator" -n . && echo "- Decorator" >> analysis_report.txt
+
+cat analysis_report.txt
