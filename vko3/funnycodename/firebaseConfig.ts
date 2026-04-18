7e4e6a358831b4e0852c2c757c661082e39b380c
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-04-18 20:32:52 +0300

    fixed

--- vko3/funnycodename/firebaseConfig.ts
+++ vko3/funnycodename/firebaseConfig.ts
@@ -1,12 +1,11 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlJqlIflyLzQAf_UJ5GOjHJG8hGgbQn7w",
-  authDomain: "vite-webpilvi.firebaseapp.com",
-  projectId: "vite-webpilvi",
-  storageBucket: "vite-webpilvi.firebasestorage.app",
-  messagingSenderId: "731985893670",
-  appId: "1:731985893670:web:247f2904914b49107289e7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- vko3/funnycodename/src/App.tsx
+++ vko3/funnycodename/src/App.tsx
@@ -1,4 +1,4 @@
-import { cache, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { onAuthStateChanged, type User } from 'firebase/auth';
 import LoginForm from './LoginForm';
